4873dd9340d4d4bf3fdf252983a5096c364ffa6d
Srini <sriniuk@gmail.com>
2025-07-05 06:36:54 +0100

    Fix useBookSearch reference error in App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { AuthProvider } from './hooks/useAuth.tsx';
 import AppContent from './components/AppContent';
 
 function App() {
-  const { isLoading, results, error, totalResults, processingTime, hasSearched, searchBooks, handleBookClick } = useBookSearch();
   return (
     <AuthProvider>
       <AppContent />
